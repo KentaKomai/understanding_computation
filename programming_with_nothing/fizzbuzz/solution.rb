7273f359781a7e5dba9b7895ae988d5654420574
Kenta Komai <kaede06152000@gmail.com>
2015-12-13 23:43:45 +0900

    implements MOD

--- programming_with_nothing/fizzbuzz/solution.rb
+++ programming_with_nothing/fizzbuzz/solution.rb
@@ -12,23 +12,62 @@ HUNDRED = -> p { -> x { p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[
 # Boolean
 TRUE  = -> x { -> y {x}}
 FALSE = -> x { -> y {y}}
-
-# IF statement
-IF = -> b {
-  -> x {
-    -> y {
-      b[x][y]
-    }
-  }
-}
+IF = -> b { b }
 
 p IF[TRUE]['happy']['sad']
 p IF[FALSE]['happy']['sad']
 
-IF = -> b { b }
 
 IS_ZERO = -> n { n[ -> x {FALSE}][TRUE] }
 
 PAIR  = -> x { -> y { -> f { f[x][y] } } }
 LEFT  = -> p { p[-> x { -> y { x } } ] }
 RIGHT = -> p { p[-> x { -> y { y } } ] }
+
+
+INCREMENT = -> n { -> p { -> x { p[n[p][x]] } } }
+SLIDE     = -> p { PAIR[RIGHT[p]][INCREMENT[RIGHT[p]]] }
+DECREMENT = -> n { LEFT[n[SLIDE][PAIR[ZERO][ZERO]]] }
+
+p to_integer(DECREMENT[HUNDRED])
+
+ADD      = -> m { -> n { n[INCREMENT][m] } }
+SUBTRACT = -> m { -> n { n[DECREMENT][m] } }
+MULTIPLY = -> m { -> n { n[ADD[m]][ZERO] } }
+POWER    = -> m { -> n { n[MULTIPLY[m]][ONE] } }
+
+IS_LESS_OR_EQUAL = -> m { -> n { IS_ZERO[SUBTRACT[m][n]] } }
+MOD = -> m { -> n {
+  IF[IS_LESS_OR_EQUAL[n][m]][ -> x {
+    MOD[SUBTRACT[m][n]][n][x]
+  }]
+}}
+
+MOD = -> m { -> n {
+  IF[IS_LESS_OR_EQUAL[n][m]][ -> x {
+      MOD[SUBTRACT[m][n]][n][x]
+    }
+  ][m]
+}}
+
+
+p to_integer(MOD[THREE][TWO])
+
+# Y Combinator
+# 引数のprocを"proc自体を第一引数"として呼び出す。
+Y_COMBINATOR = -> f {
+  -> x { f[ x[x] ] }[ -> x { f[ x[x] ] } ]
+}
+Z_COMBINATOR = -> f {
+  -> x { f[-> y { x[x][y] }] }[-> x { f[-> y { x[x][y] }] }]
+}
+
+MOD = Z_COMBINATOR[-> f { -> m { -> n {
+  IF[IS_LESS_OR_EQUAL[n][m]][
+    -> x {
+      f[SUBTRACT[m][n]][n][x]
+    }
+  ][m]
+}}}]
+
+p to_integer(MOD[THREE][TWO])
